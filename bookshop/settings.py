bfee314c25cbbb37326eed48b63fc9dd1b3665a9
Xwiit <switluvsangels@gmail.com>
2021-11-09 13:17:13 +0100

    'settings'

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7y=x#3#by&slsu@$$2*u#elqf(&xzo4)wh*fbwm!rymcow8tj6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
